7d7bbfb5b7d8062a00a61dbf4f179e4f22627627
Kevin Daněk <heindrich73@gmail.com>
2024-05-04 14:35:53 +0200

    feat: Better logging

--- Cvičení 1/Logger.cpp
+++ Cvičení 1/Logger.cpp
@@ -24,3 +24,36 @@ void Logger::critical(const std::string& message)
 {
 	Logger::log(Severity::CRITICAL, message);
 }
+
+void Logger::log(Severity severity, const std::string& message)
+{
+    {
+        std::time_t now = std::time(nullptr);
+        std::tm tm_now;
+        localtime_s(&tm_now, &now);
+
+        std::cout << "[" << std::put_time(&tm_now, "%Y-%m-%d %H:%M:%S") << "] ";
+
+        switch (severity) {
+        case Severity::DEBUG:
+            std::cout << "[DEBUG]";
+            break;
+        case Severity::INFO:
+            std::cout << "[INFO]";
+            break;
+        case Severity::WARNING:
+            std::cout << "[WARNING]";
+            break;
+        case Severity::ERR:
+            std::cout << "[ERROR]";
+            break;
+        case Severity::CRITICAL:
+            std::cout << "[CRITICAL]";
+            break;
+        }
+
+        std::cout << "\t";
+        std::cout << message;
+        std::cout << std::endl;
+    }
+}
--- Cvičení 1/Logger.h
+++ Cvičení 1/Logger.h
@@ -22,32 +22,6 @@ public:
     static void critical(const std::string& message);
 
 private:
-    static void log(Severity severity, const std::string& message) {
-        std::time_t now = std::time(nullptr);
-        std::tm tm_now;
-        localtime_s(&tm_now, &now);
-
-        std::cout << "[" << std::put_time(&tm_now, "%Y-%m-%d %H:%M:%S") << "] ";
-
-        switch (severity) {
-        case Severity::DEBUG:
-            std::cout << "[DEBUG] ";
-            break;
-        case Severity::INFO:
-            std::cout << "[INFO] ";
-            break;
-        case Severity::WARNING:
-            std::cout << "[WARNING] ";
-            break;
-        case Severity::ERR:
-            std::cout << "[ERROR] ";
-            break;
-        case Severity::CRITICAL:
-            std::cout << "[CRITICAL] ";
-            break;
-        }
-
-        std::cout << message << std::endl;
-    }
+    static void log(Severity severity, const std::string& message);
 };
 
